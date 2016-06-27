b41311c504ca5c5c88fd954edcfb14ff17b6623c
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2016-06-27 23:32:41 +0200

    Images are now downloaded and their links are updated

--- SquareSpaceToJekyll/SquareSpaceToJekyll/Program.cs
+++ SquareSpaceToJekyll/SquareSpaceToJekyll/Program.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 using SquareSpaceToJekyll.Model;
 
@@ -29,20 +32,16 @@ namespace SquareSpaceToJekyll
             }
 
             // Blog posts
-            var postsPath = Directory.CreateDirectory(Path.Combine(pathToJekyllSite, "_posts")).FullName;
-            var draftsPath = Directory.CreateDirectory(Path.Combine(pathToJekyllSite, "_drafts")).FullName;
-
-            XNamespace wpNS = "http://wordpress.org/export/1.2/";
-            XNamespace contentNS = "http://purl.org/rss/1.0/modules/content/";
-            var blogPosts = children.Where(i => i.Name == "item" && (string)i.Element(wpNS + "post_type") == "post");
+            var io = new IO(pathToJekyllSite);
+            var blogPosts = children.Where(i => i.Name == "item" && (string)i.Element(Namespaces.wpNS + "post_type") == "post");
             foreach (var xmlBlogPost in blogPosts) {
                 BlogPost blogPost;
-                if (xmlBlogPost.Element(wpNS + "postmeta")?.Element(wpNS + "meta_key")?.Value == "passthrough_url") {
-                    blogPost = new LinkPost {
-                        ExternalLink = xmlBlogPost.Element(wpNS + "postmeta").Element(wpNS + "meta_value").Value
+                if (xmlBlogPost.Element(Namespaces.wpNS + "postmeta")?.Element(Namespaces.wpNS + "meta_key")?.Value == "passthrough_url") {
+                    blogPost = new LinkPost(xmlBlogPost, io) {
+                        ExternalLink = xmlBlogPost.Element(Namespaces.wpNS + "postmeta").Element(Namespaces.wpNS + "meta_value").Value
                     };
                 } else {
-                    blogPost = new BlogPost();
+                    blogPost = new BlogPost(xmlBlogPost, io);
                 }
 
                 blogPost.Title = xmlBlogPost.Element("title").Value;
@@ -52,17 +51,19 @@ namespace SquareSpaceToJekyll
                     blogPost.AddTag(tag);
                 }
 
-                blogPost.Content = xmlBlogPost.Element(contentNS + "encoded").Value;
-                var savePath = xmlBlogPost.Element(wpNS + "status").Value == "draft" ? draftsPath : postsPath;
-                blogPost.Save(savePath);
+                blogPost.Content = xmlBlogPost.Element(Namespaces.contentNS + "encoded").Value;
 
-                // TODO: Download and relink images
+                blogPost.Save();
             }
 
             // TODO: Other pages
         }
 
         public class LinkPost : BlogPost {
+            public LinkPost(XElement blogPostXML, IO io) : base(blogPostXML, io) {
+
+            }
+
             public string ExternalLink { get; set; }
 
             public override string Content {
@@ -76,20 +77,41 @@ namespace SquareSpaceToJekyll
         }
 
         public class BlogPost {
+            readonly IO io;
+
             readonly string layout;
             readonly List<string> tags;
 
             protected readonly StringBuilder content;
 
-            public BlogPost() {
+            Image[] images;
+
+            public BlogPost(XElement blogPostXML, IO io) {
+                this.io = io;
+                IsDraft = blogPostXML.Element(Namespaces.wpNS + "status").Value == "draft";
+
                 layout = "post";
                 tags = new List<string>();
 
                 content = new StringBuilder("---");
+                images = new Image[0];
             }
 
+            public bool IsDraft { get; }
+
             public string Title { get; set; }
-            public string Link { get; set; }
+
+            public string WebSafeTitle { get; private set; }
+
+            public string Link {
+                set {
+                    var dateParts = value.Split('/');
+                    NameWithDate = $"{dateParts[2]}-{dateParts[3]}-{dateParts[4]}-{dateParts[5]}";
+                    WebSafeTitle = dateParts[5];
+                }
+            }
+
+            public string NameWithDate { get; private set; }
 
             public virtual string Content {
                 get {
@@ -97,6 +119,7 @@ namespace SquareSpaceToJekyll
                 }
 
                 set {
+                    Console.WriteLine($"Parsing {Title}");
                     content.AppendLine();
                     content.Append("layout: ");
                     content.AppendLine(layout);
@@ -112,19 +135,117 @@ namespace SquareSpaceToJekyll
                     content.AppendLine("---");
                     var normalizedContent = Regex.Replace(value, @"\[caption id="".*"" align="".*"" width="".*""\]", "");
                     normalizedContent = normalizedContent.Replace("[/caption]", "");
+
+                    var html = new HtmlAgilityPack.HtmlDocument();
+                    html.LoadHtml(normalizedContent);
+
+                    var imageTags = html.DocumentNode.SelectNodes("//img");
+                    if (imageTags != null) {
+                        images = new Image[imageTags.Count];
+                        Parallel.For(0, imageTags.Count, i => {
+                            var src = imageTags[i].Attributes["src"];
+                            var imageUrl = src.Value;
+                            if (!imageUrl.StartsWith("http", StringComparison.InvariantCulture)) {
+                                var normalizedImageUrl = imageUrl.StartsWith("/", StringComparison.InvariantCulture) ? imageUrl.Remove(0, 1) : imageUrl;
+                                imageUrl = UserSettings.InternalLinkPrefix + normalizedImageUrl;
+                            }
+                            
+                            Console.WriteLine($"Downloading {imageUrl}...");
+
+                            using (var httpClient = new HttpClient()) {
+                                httpClient.MaxResponseContentBufferSize = 2000000L;
+                                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+
+                                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+                                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17");
+
+                                var request = new HttpRequestMessage() {
+                                    RequestUri = new Uri(imageUrl),
+                                    Method = HttpMethod.Get,
+                                };
+
+                                var getTask = httpClient.SendAsync(request);
+                                getTask.Wait();
+                                var response = getTask.Result;
+                                response.EnsureSuccessStatusCode();
+                                var jsonTask = response.Content.ReadAsByteArrayAsync();
+                                jsonTask.Wait();
+                                images[i] = new Image($"/{UserSettings.ImageFolder}/{WebSafeTitle}/{i + Path.GetFileName(imageUrl)}", jsonTask.Result);
+                            }
+
+                            src.Value = images[i].Source;
+                        });
+                    }
+
+                    var sb = new StringBuilder();
+                    var sw = new StringWriter(sb);
+                    html.Save(sw);
+                    normalizedContent = sb.ToString();
                     content.AppendLine(normalizedContent);
                 }
             }
+
+            public Image[] Images { get { return images; } }
 
             public void AddTag(string tag) {
                 tags.Add(tag);
             }
 
-            public void Save(string basePath) {
-                var dateParts = Link.Split('/');
-                var fileName = $"{dateParts[2]}-{dateParts[3]}-{dateParts[4]}-{dateParts[5]}";
-                File.WriteAllText(Path.Combine(basePath, fileName) + ".html", Content);
+            public void Save() {
+                io.Save(this);
             }
+        }
+
+        public class IO {
+            readonly string postsPath;
+            readonly string draftsPath;
+
+            public IO(string pathToJekyllSite) {
+                postsPath = Directory.CreateDirectory(Path.Combine(pathToJekyllSite, "_posts")).FullName;
+                draftsPath = Directory.CreateDirectory(Path.Combine(pathToJekyllSite, "_drafts")).FullName;
+                ImagesPath = Directory.CreateDirectory(Path.Combine(pathToJekyllSite, UserSettings.ImageFolder)).FullName;
+            }
+
+            public string ImagesPath { get; }
+
+            public void Save(BlogPost blogPost) {
+                SaveBlogPost(blogPost);
+                SaveImages(blogPost);
+            }
+
+            void SaveBlogPost(BlogPost blogPost) {
+                var savePath = blogPost.IsDraft ? draftsPath : postsPath;
+                File.WriteAllText(Path.Combine(savePath, blogPost.NameWithDate) + ".html", blogPost.Content);
+            }
+
+            void SaveImages(BlogPost blogPost) {
+                foreach (var image in blogPost.Images) {
+                    var imageDirForPost = Path.Combine(ImagesPath, blogPost.WebSafeTitle);
+                    Directory.CreateDirectory(imageDirForPost);
+                    File.WriteAllBytes(Path.Combine(imageDirForPost, image.Name), image.Content);
+                }
+            }
+        }
+
+        public struct Image {
+            public Image(string source, byte[] content) {
+                Source = source;
+                Content = content;
+            }
+
+            public string Source { get; }
+            public string Name => Path.GetFileName(Source);
+            public byte[] Content { get; }
+        }
+
+        public static class UserSettings {
+            public const string ImageFolder = "img";
+            public const string InternalLinkPrefix = "https://runar-ovesenhjerpbakk.squarespace.com/";
+        }
+
+        public static class Namespaces {
+            public static readonly XNamespace wpNS = "http://wordpress.org/export/1.2/";
+            public static readonly XNamespace contentNS = "http://purl.org/rss/1.0/modules/content/";
         }
     }
 }
